36c6ef13792bba7a521873f5d57b23e4c749fc23
Ashraf Hijazi <74490789+Ashrafhij@users.noreply.github.com>
2024-10-25 17:29:34 +0300

    BG-fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,20 +18,21 @@ const Home = () => {
       {isLoading ? (
         <div className="flex justify-center items-center min-h-screen">
           {/* Tailwind spinner loader */}
-          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
         </div>
       ) : (
         <div
-          className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white opacity-90 w-full max-w-full"
+          className="min-h-screen flex flex-col justify-center text-black w-full max-w-full lg:px-32 px-5 bg-cover bg-center relative"
           style={{
             backgroundImage: `url(${backgroundImage})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
           }}
         >
+          {/* Red overlay */}
+          <div className="absolute inset-0 bg-backgroundColor opacity-50"></div>
+
+          {/* Content */}
           <div
-            className="w-full lg:w-4/5 space-y-5 mt-[-2] lg:mt-[-4] flex flex-col items-start"
+            className="relative w-full lg:w-4/5 space-y-5 lg:mt-[-4] mt-[-2] flex flex-col items-start"
             dir="rtl"
           >
             <h1 className="text-5xl lg:text-6xl md:text-4xl sm:text-3xl font-semibold leading-tight text-right">
